32f1f0879f8b4aaeb1d514e612ae471e683be172
Mariam Agboola <mariahhm42@gmail.com>
2024-11-18 12:01:17 -0700

    index.js file was created

---
+++ index.js
@@ -0,0 +1,13 @@
+const { client } = require('./client')
+const { server } = require('./server')
+const { input } = require('./input')
+const { play } = require('./play')
+const { constants } = require('./constants')
+
+module.exports = {
+  client,
+  server,
+  input,
+  play,
+  constants
+}
